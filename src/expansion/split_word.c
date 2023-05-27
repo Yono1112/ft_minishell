b7965beb677946f44762ebebea4ece31ab4101b3
Yuma Ohno <yuohno@c1r27s10.42tokyo.jp>
2023-05-28 01:59:59 +0900

    write split_word in the middle of the word and comment out everything

--- src/expansion/split_word.c
+++ src/expansion/split_word.c
@@ -6,7 +6,7 @@
 /*   By: yuohno <yuohno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 18:11:59 by yuohno            #+#    #+#             */
-/*   Updated: 2023/05/27 18:39:29 by yuohno           ###   ########.fr       */
+/*   Updated: 2023/05/28 01:56:46 by yuohno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,26 @@
 
 // void	split_word_token(t_token *token)
 // {
+// 	char	*current_word;
+// 	char	*new_word;
 // 
+// 	while (token != NULL && token->kind == TK_WORD && token->word != NULL)
+// 	{
+// 		current_word = token->word;
+// 		new_word = NULL;
+// 		while (*current_word != '\0')
+// 		{
+// 			if (*current_word == SINGLE_QUOTE_CHAR || *current_word == DOUBLE_QUOTE_CHAR)
+// 				remove_single_double_quote(&new_word, &current_word, current_word);
+// 			else
+// 				append_char(&new_word, *current_word++);
+// 		}
+// 		free(token->word);
+// 		token->word = new_word;
+// 		token = token->next;
+// 	}
 // }
-
+// 
 // void	split_word(t_node *node)
 // {
 // 	while (node != NULL)
