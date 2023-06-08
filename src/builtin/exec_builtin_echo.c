5e53b6f11b28259b2ec6a98c13be6852aaa09ad5
Yono1112 <yuma.ohno.sss@gmail.com>
2023-06-08 15:16:47 +0900

    refactor exec_builtin_echo() and add echo-tester in test.sh

--- src/builtin/exec_builtin_echo.c
+++ src/builtin/exec_builtin_echo.c
@@ -1,40 +1,38 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+
 #include "minishell.h"
 
-static int	count_arg(char **argv)
+static int	count_argc(char **argv)
 {
 	int i;
 
 	i = 1;
 	while (argv[i])
 		i++;
-	return i;
+	return (i);
 }
 
-static int	check_valid_option(char *second_arg)
+static bool	check_valid_option(char *second_arg)
 {
-	int i;
+	int	i;
 
 	i = 1;
 	while (second_arg[i])
 	{
 		if (second_arg[i] != 'n')
-			return (1);
+			return (false);
 		i++;
 	}
-	return (0);
+	return (true);
 }
 
-void	write_str(char *str)
+static void	write_str(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i])
 	{
-		write(1, str + i, 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 }
@@ -42,40 +40,29 @@ void	write_str(char *str)
 
 int	exec_builtin_echo(char **argv)
 {
-	int i;
-	int flag;
-	int argc;
+	int 	i;
+	int 	is_option;
+	int 	argc;
 
 	i = 0;
-	flag = 0;
-	argc = count_arg(argv);
+	is_option = 0;
+	argc = count_argc(argv);
 	if (argc == 1)
-	{
 		printf("\n");
-		return (0);
-	}
-	if ('-' == argv[1][0])
+	else
 	{
-		if (!check_valid_option(argv[1]))
-			flag = 1;
+		if ('-' == argv[1][0] && check_valid_option(argv[1]))
+			is_option = 1;
+		i = is_option + 1;
+		while (i < argc)
+		{
+			write_str(argv[i]);
+			if (i + 1 < argc)
+				write(1, " ", 1);
+			i++;
+		}
+		if (!is_option)
+			printf("\n");
 	}
-	i = flag + 1;
-	while (i < argc)
-	{
-		write_str(argv[i]);
-		if (i + 1 < argc)
-			write(1, " ", 1);
-		i++;
-	}
-	if (!flag)
-		printf("\n");
 	return (0);
 }
-
-//int main(int argc, char **argv)
-//{
-//	if (argc < 1)
-//		return 1;
-//	exec_builtin_echo(argv + 1);
-//}
-
