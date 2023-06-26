7dfdf93bd0688953a24858b452861c3e516f4afa
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-06-26 17:50:32 +0900

    norminette builtin

--- src/builtin/is_builtin.c
+++ src/builtin/is_builtin.c
@@ -6,7 +6,7 @@
 /*   By: rnaka <rnaka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 18:28:23 by yuohno            #+#    #+#             */
-/*   Updated: 2023/06/26 16:19:32 by rnaka            ###   ########.fr       */
+/*   Updated: 2023/06/26 17:50:02 by rnaka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 bool	is_builtin(t_node *node)
 {
-	char *const	builtin_commands[] = {"exit", "echo", "export", "unset", "env","cd","pwd"};
+	char *const	builtin_commands[]
+		= {"exit", "echo", "export", "unset", "env","cd","pwd"};
 	size_t		i;
 	size_t		builtin_len;
 	char		*builtin_command;
 
-	if (node != NULL && node->command != NULL && node->command->args != NULL && node->command->args->word != NULL)
+	if (node != NULL && node->command != NULL
+		&& node->command->args != NULL && node->command->args->word != NULL)
 	{
 		builtin_command = node->command->args->word;
 		builtin_len = sizeof(builtin_commands) / sizeof(*builtin_commands);
