8120c6949ac07b921356d92c082d9c70c6c19db7
Yuma Ohno <yuohno@c1r27s10.42tokyo.jp>
2023-06-29 12:02:30 +0900

    remove return statement in reset_redirect()

--- src/redirection/reset_redirect.c
+++ src/redirection/reset_redirect.c
@@ -6,7 +6,7 @@
 /*   By: yuohno <yuohno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 01:14:09 by yuohno            #+#    #+#             */
-/*   Updated: 2023/06/27 11:13:36 by yuohno           ###   ########.fr       */
+/*   Updated: 2023/06/29 11:58:47 by yuohno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ void	reset_redirect(t_node *redirect)
 	if (is_redirect(redirect))
 	{
 		if (dup2(redirect->stashed_std_fd, redirect->std_fd) < 0)
-		{
 			fatal_error("dup2");
-			return ;
-		}
 	}
 	else
 		fatal_error("reset_redirect");
