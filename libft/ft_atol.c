3cd5920c728ce84304052786a146561b7dc46c58
Yuma Ohno <yuohno@c1r27s10.42tokyo.jp>
2023-06-29 16:12:55 +0900

    fix norminette in ft_atol and exec_builtin_exit

--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -6,20 +6,26 @@
 /*   By: yuohno <yuohno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 12:05:26 by yuohno            #+#    #+#             */
-/*   Updated: 2023/06/29 13:28:41 by yuohno           ###   ########.fr       */
+/*   Updated: 2023/06/29 16:10:11 by yuohno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check_overflow(char c, int sign, long result)
+static int	check_overflow(char c, int sign, long result, int *is_over_long)
 {
 	if (sign == 1 && (result > LONG_MAX / 10 || (result == LONG_MAX / 10
 				&& c - '0' > LONG_MAX % 10)))
+	{
+		*is_over_long = 1;
 		return (1);
+	}
 	if (sign == -1 && (result > LONG_MAX / 10 || (result == LONG_MAX / 10
 				&& c - '0' > LONG_MAX % 10 + 1)))
+	{
+		*is_over_long = 1;
 		return (1);
+	}
 	return (0);
 }
 
@@ -42,16 +48,10 @@ long	ft_atol(const char *str, int *is_over_long)
 	}
 	while ((str[i] >= '0' && str[i] <= '9') && str[i])
 	{
-		if (sign == 1 && check_overflow(str[i], sign, result))
-		{
-			*is_over_long = 1;
+		if (sign == 1 && check_overflow(str[i], sign, result, is_over_long))
 			return (LONG_MAX);
-		}
-		if (sign == -1 && check_overflow(str[i], sign, result))
-		{
-			*is_over_long = 1;
+		if (sign == -1 && check_overflow(str[i], sign, result, is_over_long))
 			return (LONG_MIN);
-		}
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
